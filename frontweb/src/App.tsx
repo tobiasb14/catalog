10f7402233fbdc0bf9efe94973ba2afaa90786d7
tobias <tobias.barbosa@efabrika.com.br>
2022-02-28 02:10:05 -0300

    navbar styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,9 +1,13 @@
+import Navbar from 'components/Navbar';
 import './Assets/Styles/custom.scss';
 import './App.css';
 
 function App() {
   return (
+    <>
+    <Navbar />
     <h1>hello catalog</h1>
+    </>
   );
 }
 
